725c94a7c6b8a0dec9edf1bef3f0aba744fe5101
I1HN <t.lins@edu.senai.br>
2026-03-11 10:38:12 -0300

    Criados arquivos index.html e script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let oxigenio = Number(prompt ("Insira a quantidade de oxigênio restante (em litros):"));
+let pessoas = Number(prompt("Insira a quantidade de pessoas em marte:"));
+const consumoDiario = 15; // litros por pessoa.
+
+// consumo total é baseado na quantidade de pessoas multiplicado pelo consumo diário por pessoa.
+let consumoTotalBase = pessoas * consumoDiario;
+let diasRestantes = oxigenio / consumoTotalBase;
+
+alert(`Relatório de Marte: Restam ${diasRestantes.toFixed(0)} dias de oxigênio!!`);
